b49c21c3d3854c8a71c752442bdd5121b194d99f
Noora Liv <j.czinzoll@ingressy.de>
2025-09-24 11:46:02 +0200

    Finished Muss Things
    Snackautomat Anforderungen Basis
    Das System muss
    1.)Dem Benutzer ein Menü mit mindestens 3 verschiedenen Snack-Optionen anzeigen. Die Snack-Option hat eine Nummer, einen Namen ( z.B. legger Sniggors) und einen Preis (1,50€)
    2.)In einer Dauerschleife laufen. Nach einer erfolgreichen oder nicht-erfolgreichen Transaktion fängt das Programm wieder von vorne an
    3.)den Benutzer zur Geldeingabe auffordern. Dies ist eine Eingabe, in der der Nutzer einen beliebigen Wert Geld über die Konsole eingeben kann
    4.)dem Benutzer solange zur Zahlung auffordern, bis der Betrag ausreicht. Wird überzahlt, so gibt es Wechselgeld aus. Das Wechselgeld wird über die Konsole zurückgegeben (z.B. 0,50€ zurück).
    5.)gibt das Produkt aus. Dies kann auf der Konsole eine Ausgabe sein wie “hier ist dein Produkt [NAME]

--- snackautomat.java
+++ snackautomat.java
@@ -1,28 +1,28 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class snackautomat {
     public static void main(String[] args) {
         boolean run = true;
+        byte arrayauswahl = -1;
+        double[] preise = {1.50, 1.08, 1.50, 1.00, 4.00};
+        String[] items = {"Waffel","Random Katjes", "Monster Energy", "Kaffee", "3er Kondom"};
 
         while (run) {
             byte menuauswahl = menu();
-            if (menuauswahl == 9)  {
+            if (menuauswahl == 9) {
                 System.out.print("Vielen Dank für deine Benutzung!");
                 run = !run;
-            } else if (menuauswahl == 1 ) {
-                System.out.println("Eine Waffel. OK.");
-                boolean bezahlvorgang = bezahlen(1.50);
-                if  (bezahlvorgang == false) {
+            } else if (menuauswahl > 0 || menuauswahl < 5) {
+                arrayauswahl =  --menuauswahl;
+                System.out.println("Eine "+ items[arrayauswahl] + ". OK.");
+                boolean bezahlvorgang = bezahlen(preise[arrayauswahl]);
+                if  (!bezahlvorgang) {
                     System.out.println("Irgendwas ist schiefgelaufen, bitte rufe die Hotline an!\nTel.: 42 42 42 42 ... Error Printing Lines!");
                     run = !run;
+                } else if (bezahlvorgang) {
+                    System.out.println("Hier ist deine "+items[arrayauswahl]+"! OK.");
                 }
-
-            }else if (menuauswahl == 2 ) {
-                System.out.println("Auswahl: " + menuauswahl);
-            }else if (menuauswahl == 3 ) {
-                System.out.println("Auswahl: " + menuauswahl);
-            }else if (menuauswahl == 4 ) {
-                System.out.println("Auswahl: " + menuauswahl);
             }else if (menuauswahl < 0 ) {
                 System.out.println("Bitte wiederhole deinen Eingabe!");
                 menu();
@@ -38,33 +38,45 @@ public class snackautomat {
         Scanner menuein = new Scanner(System.in);
 
         System.out.println("--- SNACKAUTOMAT 3000 ---");
-        System.out.println("1. Waffel (1,50€)\n2. Random Katjes (1,08€)\n3. Monster Energy (1,50€)\n4. Kaffee (1,00€)");
+        System.out.println("1. Waffel (1,50€)\n2. Random Katjes (1,08€)\n3. Monster Energy (1,50€)\n4. Kaffee (1,00€)\n5. Conrads 3er Kondom (4,00€)");
         System.out.println("9. Automaten zu beenden.");
         System.out.print("Eigabe: ");
-        auswahl = menuein.nextByte();
+        try {
+            auswahl = menuein.nextByte();
+            return auswahl;
+        } catch (InputMismatchException e) {
+            System.out.println("Bitte gebe nur Zahlen ein!");
+            return -1;
+        }
 
-        return auswahl;
     }
+
     protected static boolean bezahlen(double preis) {
         double geld = -1;
         double rueckgeld = -1;
-        double newgeld = -1;
 
         Scanner geldin = new Scanner(System.in);
 
         System.out.print("Zu Bezahlen: €"+ preis + " - Bitte Geld einwerfen, bitte KOMMAZAHLEN: ");
         geld = geldin.nextDouble();
-        System.out.println("Geld gegeben: €" + geld);
-        if (geld > preis) {
-            rueckgeld = geld - preis;
-            System.out.println("Rückgeld: €" + rueckgeld);
-            return true;
-        } else if (geld < preis) {
-            newgeld = preis - geld;
-            bezahlen(newgeld);
-        } else if (geld == preis) {
+        if (geld < 0) {
+            System.out.println("OK. Bitte kein negatives Geld einwerfen. Dieses ist nicht möglich!");
+             return bezahlen(preis);
+        } else if (geld > 2) {
+            System.out.println("OK. Bitte kein falsch Geld einwerfen. Dieses ist verboten!");
+             return bezahlen(preis);
+        } else {
+            System.out.println("Geld gegeben: €" + geld);
+            if (geld > preis) {
+                rueckgeld = geld - preis;
+                System.out.println("Rückgeld: €" + rueckgeld);
+                return true;
+            } else if (geld < preis) {
+                return bezahlen((preis-geld));
+            } //else if (geld == preis) {
+              //  return true;
+            //}
             return true;
         }
-        return false;
     }
 }
